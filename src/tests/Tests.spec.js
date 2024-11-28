7c1da3e3201cc624bfbab030aa97b4b523a18c24
Higor Goulart Massiroli <higorgoulart@hotmail.com>
2024-11-28 20:49:07 -0300

    initial commit

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -4,18 +4,19 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
+export const getPopulationDuration = new Trend('get_population_duration', true);
 export const RateContentOK = new Rate('content_OK');
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
+    getPopulationDuration: ['p(95)<5700'],
+    http_req_failed: ['rate<0.12'],
     content_OK: ['rate>0.95']
   },
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
+    { duration: '1m', target: 10 },
+    { duration: '2m', target: 300 },
+    { duration: '2m', target: 0 }
   ]
 };
 
@@ -27,23 +28,17 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
-
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-
+  const baseUrl =
+    'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
+  const params = { headers: { 'Content-Type': 'application/json' } };
   const OK = 200;
 
-  const res = http.get(`${baseUrl}`, params);
+  const res = http.get(baseUrl, params);
 
-  getContactsDuration.add(res.timings.duration);
-
+  getPopulationDuration.add(res.timings.duration);
   RateContentOK.add(res.status === OK);
 
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'GET Population - Status 200': () => res.status === OK
   });
 }
